843c3462addb548f5d608811fbd99698c1408089
Tuahta Ramadhani <59135441+taraid97exe@users.noreply.github.com>
2024-11-26 11:14:47 +0700

    Add files via upload

---
+++ final-assessment/01-writing-comments/index.js
@@ -0,0 +1,5 @@
+/* username: tara_id97exe */
+
+/* Goal tahun ini:
+1. Belajar JavaScript.
+2. Menjadi Front-End atau Back-End Developer. */
---
+++ final-assessment/02-code-style/index.js
@@ -0,0 +1,53 @@
+const books = {}
+
+function getBooks() {
+    return books;
+}
+
+function getBookById(id) {
+  let book = books[id]
+
+    if (!book) {
+      return null;
+    }
+
+    return book.id
+}
+
+function saveBook(book) {
+  books[book.id] = book
+}
+
+saveBook({id: 'book-1', name: 'Book 1'});
+let myBooks = getBooks()
+const myBook = getBookById('book-1');
+
+console.log(myBooks);
+console.log(myBook);
+
+// const books = {};
+
+// function getBooks() {
+//   return books;
+// }
+
+// function getBookById(id) {
+//   let book = books[id];
+
+//   if (!book) {
+//     return null;
+//   }
+
+//   return book.id;
+// }
+
+// function saveBook(book) {
+//   books[book.id] = book;
+// }
+
+// saveBook({ id: "book-1", name: "Book 1" });
+// let myBooks = getBooks();
+// const myBook = getBookById("book-1");
+
+// console.log(myBooks);
+// console.log(myBook);
