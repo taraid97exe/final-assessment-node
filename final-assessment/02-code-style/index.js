35313544bf472af647e5b7e7294a3c5b69de4644
taraid97exe <tuahtaramadhani@gmail.com>
2024-11-28 08:16:34 +0700

    done tugas 2

--- final-assessment/02-code-style/index.js
+++ final-assessment/02-code-style/index.js
@@ -1,53 +1,24 @@
-const books = {}
+const books = {}; /* Terdapat semicolon `;` di setiap akhir statement. */
 
 function getBooks() {
-    return books;
+  return books; /* Indentasi sebesar **2 spasi**. */
 }
 
 function getBookById(id) {
-  let book = books[id]
-
-    if (!book) {
-      return null;
-    }
-
-    return book.id
+  let book = books[id]; /* Terdapat semicolon `;` di setiap akhir statement. */ /* Indentasi sebesar **2 spasi**. */
+  if (!book) { /* Indentasi sebesar **2 spasi**. */
+    return null; /* Indentasi sebesar **2 spasi**. */
+  } /* Indentasi sebesar **2 spasi**. */
+  return book.id; /* Terdapat semicolon `;` di setiap akhir statement. */ /* Indentasi sebesar **2 spasi**. */
 }
 
 function saveBook(book) {
-  books[book.id] = book
+  books[book.id] = book; /* Terdapat semicolon `;` di setiap akhir statement. */ /* Indentasi sebesar **2 spasi**. */
 }
 
-saveBook({id: 'book-1', name: 'Book 1'});
-let myBooks = getBooks()
+saveBook({ id: 'book-1', name: 'Book 1' }); /* Selalu tambahkan spasi di antara tanda `{ }` dan nama properti yang didefinsikan dalam satu baris. */
+let myBooks = getBooks(); /* Terdapat semicolon `;` di setiap akhir statement. */
 const myBook = getBookById('book-1');
 
 console.log(myBooks);
 console.log(myBook);
-
-// const books = {};
-
-// function getBooks() {
-//   return books;
-// }
-
-// function getBookById(id) {
-//   let book = books[id];
-
-//   if (!book) {
-//     return null;
-//   }
-
-//   return book.id;
-// }
-
-// function saveBook(book) {
-//   books[book.id] = book;
-// }
-
-// saveBook({ id: "book-1", name: "Book 1" });
-// let myBooks = getBooks();
-// const myBook = getBookById("book-1");
-
-// console.log(myBooks);
-// console.log(myBook);
