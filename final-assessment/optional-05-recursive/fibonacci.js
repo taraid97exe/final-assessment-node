fa7c1d2ed727966487e160f6350409b846202630
taraid97exe <tuahtaramadhani@gmail.com>
2024-11-28 10:51:01 +0700

    done tugas 5

--- final-assessment/optional-05-recursive/fibonacci.js
+++ final-assessment/optional-05-recursive/fibonacci.js
@@ -1,5 +1,13 @@
 function fibonacci(n) {
-
+    if(n === 0) {
+        return [0];
+    }
+    if(n === 1) {
+        return [0,1];
+    }
+    const sebelumFibonacci = fibonacci(n - 1);
+    const setelahFibonacci = sebelumFibonacci[sebelumFibonacci.length - 1] + sebelumFibonacci[sebelumFibonacci.length - 2];
+    return [...sebelumFibonacci, setelahFibonacci];
 }
 
 // Jangan hapus kode di bawah ini!
